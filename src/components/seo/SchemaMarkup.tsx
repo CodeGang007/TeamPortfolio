17891cc63600042a2c81addce0293f8a4955cec3
Gourav Chakraborty <21052759@kiit.ac.in>
2026-01-19 11:28:34 +0530

    feat: add PWA manifest and SEO schema markup for improved discoverability.

---
+++ src/components/seo/SchemaMarkup.tsx
@@ -0,0 +1,33 @@
+import Script from 'next/script';
+
+export default function SchemaMarkup() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'CodeGang',
+    url: 'https://codegang.online',
+    logo: 'https://codegang.online/assets/cg-logo-online.png',
+    sameAs: [
+      'https://twitter.com/codegang',
+      'https://linkedin.com/company/codegang',
+      'https://github.com/codegang'
+    ],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      telephone: '',
+      contactType: 'customer service',
+      areaServed: 'Worldwide',
+      availableLanguage: 'English'
+    },
+    description: 'CodeGang is a premium digital agency architecting the future. We build high-performance web applications, scalable infrastructure, and stunning digital experiences.'
+  };
+
+  return (
+    <Script
+      id="schema-org-markup"
+      type="application/ld+json"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  );
+}
